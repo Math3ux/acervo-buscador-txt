fb05674a30ef357abec7180193e51dfaab3abab4
Matheus <matheusleidow@gmail.com>
2025-01-12 20:43:15 -0300

    Atualização do title e favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#000000" />
+        <title>Acervo Receitas</title>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
